9c3205a0cb6f12e23703c61ba04edc503895ff06
chandan <chandanshaw833609@gmail.com>
2024-09-29 14:52:16 +0530

    add hello comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 export const routes: Routes = [
     {
         path : 'hello',
-        component : AppComponent
+        component : HelloComponent
     }
 ];
